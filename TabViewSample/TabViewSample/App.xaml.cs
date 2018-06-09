ec4d9ce26d3a208977ca80e96b466883e6ed43d3
rjantz3 <rjantz3@msn.com>
2018-06-08 18:41:01 -0700

    Update App.xaml.cs

--- TabViewSample/TabViewSample/App.xaml.cs
+++ TabViewSample/TabViewSample/App.xaml.cs
@@ -13,7 +13,7 @@ namespace TabViewSample
 		{
 			InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+                        MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
